cd06824add65b605ed89631bc38ba741e662a1c6
Mikhail Boin <misoin2807@gmail.com>
2025-08-02 18:45:38 +0300

    feat: created database instance class

---
+++ src/main/kotlin/com/apiproject/taskhandler/User.kt
@@ -0,0 +1,17 @@
+package com.apiproject.taskhandler
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "users")
+data class User(
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    val id: Long,
+    val name: String,
+    val email: String,
+)
