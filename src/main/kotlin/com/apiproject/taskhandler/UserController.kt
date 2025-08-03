8b26b6673df0b22a50feab9fc486ffd780510c5e
Mikhail Boin <misoin2807@gmail.com>
2025-08-03 16:18:52 +0300

    feat: added put and delete to controller

--- src/main/kotlin/com/apiproject/taskhandler/UserController.kt
+++ src/main/kotlin/com/apiproject/taskhandler/UserController.kt
@@ -4,9 +4,12 @@ import org.apache.coyote.Response
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -28,11 +31,32 @@ class UserController(@Autowired private val userRepository: UserRepository) {
     //Get user by id
     @GetMapping("/{userId}")
     fun getUserById(@PathVariable userId: Long): ResponseEntity<User> {
-        val user = userRepository.findById(userId).orElse(null)
-        return if (user != null) {
-            ResponseEntity(user, HttpStatus.OK)
+        val user = userRepository.findById(userId)
+        return if (user.isPresent) {
+            ResponseEntity(user.get(), HttpStatus.OK)
         } else {
             ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
+
+    @PutMapping("/{userId}")
+    fun updateUserById(@PathVariable userId: Long, @RequestBody newUser: User): ResponseEntity<User> {
+        val found = userRepository.findById(userId)
+        if (found.isPresent) {
+            userRepository.save(User(userId, newUser.name, newUser.email))
+            return ResponseEntity(HttpStatus.OK)
+        } else {
+            return ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
+
+    @DeleteMapping("/{userId}")
+    fun deleteUserById(@PathVariable userId: Long): ResponseEntity<User> {
+        if (userRepository.existsById(userId)) {
+            userRepository.deleteById(userId)
+            return ResponseEntity(HttpStatus.OK)
+        } else {
+            return ResponseEntity(HttpStatus.NOT_FOUND)
         }
     }
 }
