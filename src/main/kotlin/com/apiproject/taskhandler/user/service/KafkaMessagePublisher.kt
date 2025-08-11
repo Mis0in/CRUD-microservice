9b5fec52df527cac39c83a9deb35ea14eddce6b1
Mikhail Boin <misoin2807@gmail.com>
2025-08-11 23:29:58 +0300

    feat: add publish functions

--- src/main/kotlin/com/apiproject/taskhandler/user/service/KafkaMessagePublisher.kt
+++ src/main/kotlin/com/apiproject/taskhandler/user/service/KafkaMessagePublisher.kt
@@ -1,5 +1,9 @@
 package com.apiproject.taskhandler.user.service
 
+import com.apiproject.taskhandler.user.service.events.UserEvent
+import com.apiproject.taskhandler.user.service.events.KafkaEvent
+import com.apiproject.taskhandler.user.service.events.RawEvent
+import com.apiproject.taskhandler.user.service.events.UserEventType
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -12,10 +16,40 @@ class KafkaMessagePublisher(
     @Autowired private val template: KafkaTemplate<String, Any>,
     @Autowired private val mapper: ObjectMapper
 ) {
-    private val logger: Logger = LoggerFactory.getLogger(KafkaMessagePublisher::class.java)
+    private val logger = LoggerFactory.getLogger(KafkaMessagePublisher::class.java)
 
-    fun sendMessage(topic: KafkaTopic, message: String) {
-        template.send(topic.topicName, message)
+    fun publishEvent(topic: KafkaTopic, key: String, event: KafkaEvent) {
+        val message = when (event) {
+            is UserEvent -> mapper.writeValueAsString(event)
+            is RawEvent -> event.rawData
+        }
+
+        val future = template.send(topic.topicName, key, message)
+        future.whenComplete { result, ex ->
+            if (ex != null) {
+                logger.error(
+                    "Unable to send message=[{}] with key=[{}] to topic=[{}]",
+                    message, key, topic.topicName, ex
+                )
+            } else {
+                logger.debug(
+                    "Sent message=[{}] with key=[{}] to topic=[{}] offset=[{}]",
+                    message, key, topic.topicName, result?.recordMetadata?.offset()
+                )
+            }
+        }
+
+    }
+
+    fun publishUserEvent(userId: Long, eventType: UserEventType) {
+        publishEvent(
+            topic = KafkaTopic.USER_EVENTS,
+            key = "$userId",
+            event = UserEvent(
+                userId = userId,
+                eventType = eventType,
+            ),
+        )
     }
 
 }
